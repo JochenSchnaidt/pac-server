c759e8cbdaebb4609b1d2ee320a6123c4895bdca
Jochen Schnaidt <jschnaidt@prodyna.com>
2016-08-30 23:42:21 +0200

    Javadoc added

--- voting-poc/voting-poc-application/src/main/java/com/prodyna/pac/monitoring/BusinessAspect.java
+++ voting-poc/voting-poc-application/src/main/java/com/prodyna/pac/monitoring/BusinessAspect.java
@@ -6,35 +6,40 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+/**
+ * Class contains {@code Pointcut} expressions and aspect types for the business layer.
+ *
+ * @see AbstractAspect
+ */
 @Aspect
 @Component
 public class BusinessAspect extends AbstractAspect {
 
-    @Pointcut("execution(* com.prodyna.pac.service.UserService.*(..))")
-    private void userPointcut() {
-    }
+	@Pointcut("execution(* com.prodyna.pac.service.UserService.*(..))")
+	private void userPointcut() {
+	}
 
-    @Pointcut("execution(* com.prodyna.pac.service.VoteService.*(..))")
-    private void votePointcut() {
-    }
+	@Pointcut("execution(* com.prodyna.pac.service.VoteService.*(..))")
+	private void votePointcut() {
+	}
 
-    @Pointcut("execution(* com.prodyna.pac.service.VotingService.*(..))")
-    private void votingPointcut() {
-    }
+	@Pointcut("execution(* com.prodyna.pac.service.VotingService.*(..))")
+	private void votingPointcut() {
+	}
 
-    @Around("userPointcut()")
-    public Object createUserStatistic(ProceedingJoinPoint joinPoint) throws Throwable {
-        return createStatistic(joinPoint);
-    }
+	@Around("userPointcut()")
+	public Object createUserStatistic(ProceedingJoinPoint joinPoint) throws Throwable {
+		return createStatistic(joinPoint);
+	}
 
-    @Around("votePointcut()")
-    public Object createVoteStatistic(ProceedingJoinPoint joinPoint) throws Throwable {
-        return createStatistic(joinPoint);
-    }
+	@Around("votePointcut()")
+	public Object createVoteStatistic(ProceedingJoinPoint joinPoint) throws Throwable {
+		return createStatistic(joinPoint);
+	}
 
-    @Around("votingPointcut()")
-    public Object createVotingStatistic(ProceedingJoinPoint joinPoint) throws Throwable {
-        return createStatistic(joinPoint);
-    }
+	@Around("votingPointcut()")
+	public Object createVotingStatistic(ProceedingJoinPoint joinPoint) throws Throwable {
+		return createStatistic(joinPoint);
+	}
 
 }
--- voting-poc/voting-poc-application/src/main/java/com/prodyna/pac/monitoring/PersistenceAspect.java
+++ voting-poc/voting-poc-application/src/main/java/com/prodyna/pac/monitoring/PersistenceAspect.java
@@ -6,26 +6,31 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+/**
+ * Class contains {@code Pointcut} expressions and aspect types for the persistence layer.
+ *
+ * @see AbstractAspect
+ */
 @Aspect
 @Component
 public class PersistenceAspect extends AbstractAspect {
 
-    @Pointcut("execution(* com.prodyna.pac.persistence.UserPersistenceService.*(..))")
-    private void userPointcut() {
-    }
+	@Pointcut("execution(* com.prodyna.pac.persistence.UserPersistenceService.*(..))")
+	private void userPointcut() {
+	}
 
-    @Pointcut("execution(* com.prodyna.pac.persistence.VotePersistenceService.*(..))")
-    private void votePointcut() {
-    }
+	@Pointcut("execution(* com.prodyna.pac.persistence.VotePersistenceService.*(..))")
+	private void votePointcut() {
+	}
 
-    @Around("userPointcut()")
-    public Object createUserStatistic(ProceedingJoinPoint joinPoint) throws Throwable {
-        return createStatistic(joinPoint);
-    }
+	@Around("userPointcut()")
+	public Object createUserStatistic(ProceedingJoinPoint joinPoint) throws Throwable {
+		return createStatistic(joinPoint);
+	}
 
-    @Around("votePointcut()")
-    public Object createVoteStatistic(ProceedingJoinPoint joinPoint) throws Throwable {
-        return createStatistic(joinPoint);
-    }
+	@Around("votePointcut()")
+	public Object createVoteStatistic(ProceedingJoinPoint joinPoint) throws Throwable {
+		return createStatistic(joinPoint);
+	}
 
 }
